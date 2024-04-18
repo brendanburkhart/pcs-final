2db69a7527df065436e6bb2245ada43a0530e5d0
Brendan Burkhart <brendan.f.burkhart@gmail.com>
2024-04-18 00:52:15 -0400

    Move modular inverse to separate module

--- src/classgroup.rs
+++ src/classgroup.rs
@@ -1,7 +1,7 @@
 use lazy_static::lazy_static;
 use num_bigint::BigUint;
-use num_modular::ModularCoreOps;
-use num_traits::{One, Zero};
+
+use crate::modular;
 
 // class group structure for the CSIDH-512 class group,
 // data from CSI-FiSH: https://eprint.iacr.org/2019/498
@@ -13,27 +13,6 @@ pub const PRIMES: [u16; 74] = [
    229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313,
    317, 331, 337, 347, 349, 353, 359, 367, 373, 587,
 ];
-
-fn modular_inverse(a: BigUint, n: &BigUint) -> BigUint {
-    let mut t0: BigUint = BigUint::from(0u32);
-    let mut r0: BigUint = n.clone();
-
-    let mut t1: BigUint = BigUint::from(1u32);
-    let mut r1: BigUint = a;
-
-    let zero: BigUint = Zero::zero();
-    let one: BigUint = One::one();
-
-    while r1 != zero {
-        let quotient = &r0 / &r1; // non-modular division
-        (t0, t1) = (t1.clone(), (&t0).subm((&quotient).mulm(&t1, n), n));
-        (r0, r1) = (r1.clone(), (&r0).subm((&quotient).mulm(&r1, n), n));
-    }
-
-    assert_eq!(r0, one); // if gcd is not one, we cannot invert
-
-    return t0;
-}
 
 lazy_static! {
     // p = 4 * prod(PRIMES) - 1
@@ -47,5 +26,5 @@ lazy_static! {
     (BigUint::from(0x65b48e8f740f89bfu64) * BigUint::from(2u32).pow(7 * 64));
 
     // INV4 = (1/4) mod p
-    pub static ref INV4: BigUint = modular_inverse(BigUint::from(4u32), &P);
+    pub static ref INV4: BigUint = modular::inverse(BigUint::from(4u32), &P);
 }
---
+++ src/modular.rs
@@ -0,0 +1,29 @@
+use num_bigint::{BigInt, BigUint, Sign};
+use num_traits::{One, Zero};
+
+pub fn inverse(a: BigUint, n: &BigUint) -> BigUint {
+    let mut t0: BigInt = BigInt::from(0i32);
+    let mut r0: BigInt = BigInt::from_biguint(Sign::Plus, n.clone());
+
+    let mut t1: BigInt = BigInt::from(1i32);
+    let mut r1: BigInt = BigInt::from_biguint(Sign::Plus, a.clone());
+
+    let zero: BigInt = Zero::zero();
+    let one: BigInt = One::one();
+
+    while r1 != zero {
+        let quotient = &r0 / &r1; // non-modular division
+        (t0, t1) = (t1.clone(), &t0  - (&quotient * &t1));
+        (r0, r1) = (r1.clone(), &r0 - (&quotient * &r1));
+    }
+
+    assert_eq!(r0, one); // if gcd is not one, we cannot invert
+
+    // Euclidean algorithm terminates with |t0| < n, so either 0 <= t0 < n or 0 <= (t0 + n) < n
+    if t0.sign() == Sign::Minus {
+        t0 = t0 + BigInt::from_biguint(Sign::Plus, n.clone());
+        assert_ne!(t0.sign(), Sign::Minus);
+    }
+
+    return t0.to_biguint().expect("must be positive");
+}
